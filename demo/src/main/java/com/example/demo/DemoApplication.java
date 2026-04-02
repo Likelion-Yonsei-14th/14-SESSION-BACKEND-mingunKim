ef0988b211d56abf78bbabd0688b4248914f0b35
mingun <rlaalsrjswww@yonsei.ac.kr>
2026-04-02 20:37:39 +0900

    [fix] Implement discount policy service: auto DI @Configuration ver

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.config.AppConfig;
 import com.example.demo.member.Member;
 import com.example.demo.service.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.example.demo.config.AppConfig;
 
 public class DemoApplication {
 	public static void main(String[] args) {
-		AppConfig appConfig = new AppConfig();
-		OrderService orderService = appConfig.orderService();
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		OrderService orderService = ac.getBean(OrderService.class);
 		Member member = new Member("홍길동", "VIP");
 		int result = orderService.createOrder(member, 20000);
 		System.out.println("결제 금액: " + result);
--- demo/src/main/java/com/example/demo/config/AppConfig.java
+++ demo/src/main/java/com/example/demo/config/AppConfig.java
@@ -1,14 +1,19 @@
 package com.example.demo.config;
 
-import com.example.demo.discount.RateDiscountPolicy;
+import com.example.demo.discount.FixDiscountPolicy;
 import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.service.OrderService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy(); // 여기서 선택
+        return new FixDiscountPolicy();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderService(discountPolicy());
     }
